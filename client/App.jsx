c4c6cd05bd3dfd3144ac9e2449e382d11eb83f53
dudueasy <dudueasy@126.com>
2018-08-01 00:29:31 +0800

    init

---
+++ client/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class App extends React.Component{
+
+  render(){
+    return (
+      <div> This is an App</div>
+    )
+  }
+}
---
+++ client/app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './App.jsx'
+
+ReactDom.render(<App />, document.body)
+
+
