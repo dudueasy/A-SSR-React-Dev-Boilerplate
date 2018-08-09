283ad3e07a1e01611dde31f037ec053417f16f0d
dudueasy <dudueasy@126.com>
2018-08-09 21:06:08 +0800

    配置 webpack 开发时服务器 devServer

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -2,7 +2,9 @@ const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
 // html-webpack-plugin 是一个 webpack plugin, 用于在 build 的时候在指定目录生成一个 HTML 文件
 
-module.exports = {
+const isDev = process.env.NODE_ENV === 'development'
+
+let config = {
   // 定义入口文件
   entry:{
     // 使用 path.join 来获得绝对路径
@@ -17,7 +19,7 @@ module.exports = {
 
     // 定义生产环境下(webpack输出文件), 被引用的静态资源的路径(在使用 webpack-dev-server 时并不生效)
     // 这个设置对于 服务端渲染 很重要
-    publicPath: '/public',
+    publicPath: '/public/',
   },
   module:{
     rules:[
@@ -41,3 +43,30 @@ module.exports = {
     new HTMLPlugin({template: path.join(__dirname,'../client/template.html')})
   ]
 }
+
+if  (isDev){
+
+  console.log('current env is development')
+// 配置开发环境的常用设置
+  config.devServer = {
+    // 允许局域网访问
+    host: '0.0.0.0',
+    port: '8888',
+    // 定义 devServer 服务器服务/工作的目录
+    contentBase:path.join(__dirname, '../dist'),
+    // 开启热模块替换 (需要在react app中进行配置, 否则会报错)
+    // hot: true,
+    // 错误信息显示
+    overlay: {
+      errors: true
+    },
+    publicPath : '/public/',
+    // 启用historyApiFallback, 实现前端路由
+    historyApiFallback:{
+      index:'/public/index.html'
+    },
+  }
+
+}
+
+module.exports = config
